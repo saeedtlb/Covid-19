fcfa80d3043cf718a1b00900b3ef1b6478a5ef10
saeed-tlb <saeedt7879s@gmail.com>
2020-08-11 21:43:02 +0430

    work on nav: create Header component and add html and css but not test it right now

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './Resources/styles/index.css';
+
+import Landing from './components/Home/Landing.jsx';
+
+ReactDOM.render(
+  <div className="main">
+    <Landing />
+  </div>,
+  document.getElementById('root')
+);
+
+// https://rapidapi.com/spamakashrajtech/api/corona-virus-world-and-india-data/details
