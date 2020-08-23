ce78c3f9156204d9d12ec1b9d79c52f3f7c85ddf
saeed-tlb <saeedt7879s@gmail.com>
2020-08-23 17:23:31 +0430

    create the about section completely

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="description">
+        <div className="text">
+          <div>
+            <span>what is COVID-19</span> coronacirus
+          </div>
+          <p>
+            Corona viruses are a type of virus. There are many different kinds,
+            and some cause disease. A newly identified type has caused a recent
+            outbreak of respiratory illness now called COVID-19.Lauren Sauer,
+            M.S., the director of operations with the Johns Hopkins Office of
+            Critical Event Preparedness and Response
+          </p>
+          <button>learn more</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './header_footer/Header.jsx';
 import Home from './Home.jsx';
+import About from './About';
 
 const Landing = () => {
   return (
     <div className="landing">
       <Header />
       <Home />
+      <About />
     </div>
   );
 };
