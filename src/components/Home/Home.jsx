506664526c595aba5b4373690edd7b9e5a565e6e
saeed-tlb <saeedt7879s@gmail.com>
2020-08-23 15:59:30 +0430

    finish the first page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home">
-      <div className="text">text</div>
+      <div className="text">
+        <div>
+          <span>COVID-19 Alert</span>
+          stay at home quarantine to stop corona virus
+        </div>
+        <p>
+          there is no specific medivine to prevent or treat coronavirus disease
+          (COVID-19). prople may need supportive care to .
+        </p>
+        <button>let us help</button>
+      </div>
       <div className="illustration"></div>
     </div>
   );
