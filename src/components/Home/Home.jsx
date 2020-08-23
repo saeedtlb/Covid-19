d60ce2b0c47f2fa54ff50a39ff0c0e5aa1040032
saeed-tlb <saeedt7879s@gmail.com>
2020-08-23 14:17:05 +0430

    add home image and make it a little bit look good

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="text">text</div>
+      <div className="illustration"></div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './header_footer/Header.jsx';
+import Home from './Home.jsx';
 
 const Landing = () => {
   return (
     <div className="landing">
       <Header />
+      <Home />
     </div>
   );
 };
