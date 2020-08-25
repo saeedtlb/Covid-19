d53dc494edd7463895ea9607a92e5958d1a80b7a
saeed-tlb <saeedt7879s@gmail.com>
2020-08-25 18:54:32 +0430

    style the list

--- src/components/Home/statistics/Map.jsx
+++ src/components/Home/statistics/Map.jsx
@@ -3,6 +3,37 @@ import React from 'react';
 import World from './World.jsx';
 
 const Map = () => {
+  const countries = [
+    {
+      name: 'USA',
+      flag: 'skyblue',
+      number: '844652',
+    },
+    {
+      name: 'italy',
+      flag: 'lightgreen',
+      number: '2159612',
+    },
+    {
+      name: 'china',
+      flag: 'red',
+      number: '512052',
+    },
+  ];
+
+  const renderCountries = () =>
+    countries.map((country, i) => (
+      <div className="country" key={i}>
+        <div className="name">
+          <span style={{ backgroundColor: `${country.flag}` }}></span>
+          <span>{country.name}</span>
+        </div>
+        <div className="amount">
+          {parseInt(country.number).toLocaleString()}
+        </div>
+      </div>
+    ));
+
   return (
     <div
       className="container"
@@ -15,7 +46,17 @@ const Map = () => {
       <div className="map">
         <World />
       </div>
-      <div className="list"></div>
+
+      <div className="list">
+        <div className="head">
+          <h2>live reports</h2>
+          <div className="box">
+            <span>&#60;</span>
+            <span>&#62;</span>
+          </div>
+        </div>
+        <div className="countries">{renderCountries()}</div>
+      </div>
     </div>
   );
 };
